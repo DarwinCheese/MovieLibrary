4aab9da1d28cca0c922c33a331192d17dafe4b6f
Tomas <tbaljeu@avans.nl>
2017-06-16 16:06:14 +0200

    volley toegevoegd aan android app

---
+++ Android/MovieLibrary/app/src/main/java/com/example/tomas/movielibrary/Domain/Config.java
@@ -0,0 +1,10 @@
+package com.example.tomas.movielibrary.Domain;
+
+/**
+ * Created by Tomas on 16-6-2017.
+ */
+
+public class Config {
+
+    public static final String URL_LOGIN = "https://node-movielibrary.herokuapp.com/api/v1/login";
+}
---
+++ Android/MovieLibrary/app/src/main/java/com/example/tomas/movielibrary/RegisterActivity.java
@@ -0,0 +1,8 @@
+package com.example.tomas.movielibrary;
+
+/**
+ * Created by Tomas on 15-6-2017.
+ */
+
+public class RegisterActivity {
+}
