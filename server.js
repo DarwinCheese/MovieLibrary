40d743db0b0a7f1d94c9ed239b012fd2896fb43a
Darwin Gutierrez <darwincheese@gmail.com>
2017-06-19 14:28:41 +0200

    minor changes

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var db = require('./config/db');
 var expressJWT = require('express-jwt');
 var app = express();
 
+
 app.use(bodyParser.urlencoded({ 'extended': 'true' })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
